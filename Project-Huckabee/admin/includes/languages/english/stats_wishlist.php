8502a68853e95d2a1d1df054b763e493a572640d
datazen <mycoderocks@gmail.com>
2018-02-08 16:42:23 -0500

    moved

--- Project-Huckabee/admin/includes/languages/english/stats_wishlist.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('TEXT_OF_CUSTOMER','Customer');
-define('TEXT_OF_PRODUCTS_LIST','Products in List');
-define('TEXT_OF_WISHLIST','Wishlist Report');
-define('TEXT_OF_PRODUCT','Product');
-define('TEXT_OF_MODEL','Model');
-define('TEXT_OF_PRICE','Price');
-?>
